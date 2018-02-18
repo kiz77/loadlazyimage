2e1c92c671a0f97875e443bf24260f1b4e0d3666
sam watttoo <sam927752@gmail.com>
2018-02-18 00:35:38 -0800

    index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports.lazyload = require('fs').readFileSync(__dirname + '/lazyload.min.js', 'utf8');
+module.exports.lazyloadDebug = require('fs').readFileSync(__dirname + '/lazyload.js', 'utf8');
